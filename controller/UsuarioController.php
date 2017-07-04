730b55d63dd749624f69e1447268f05c388fd3a9
N1KoLach0 <N1KoLach0@KoLach0>
2017-07-04 13:18:57 -0500

    Añadiendo funcion de listar

---
+++ controller/UsuarioController.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once (FOLDER_PROJECT . "/config/context.php");
+require_once (FOLDER_PROJECT . "/model/Usuario.php");
+
+
+
+$usuario = new Usuario();
+$resultado = $usuario->consultar();
+
+
+
+
