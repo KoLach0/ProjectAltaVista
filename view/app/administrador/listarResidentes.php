8461ef9bc38a323e69103d2c7ae33d04ecfcd561
kiler1313 <kilerleper@gmail.com>
2017-07-05 01:21:06 -0500

    Version 2.1	 Corregida la vista

--- view/app/administrador/listarResidentes.php
+++ view/app/administrador/listarResidentes.php
@@ -5,7 +5,7 @@
         <?php
         include_once ("../../../config/context.php");
         include(FOLDER_VIEW . "/template/head.php");
-        require_once (FOLDER_PROJECT . "/controller/UsuarioController.php");
+        require_once (FOLDER_PROJECT . "/controller/usuarioController.php");
         ?>
     </head>
 
